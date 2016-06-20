73a923f257c0d060aee638e14da55ac3209cce25
Stephen Adams <sa597@kent.ac.uk>
2016-06-20 11:04:42 +0100

    Finished training code for training a perceptron to determine if a point is above a given line.

--- LinearTraining.hs
+++ LinearTraining.hs
@@ -2,6 +2,7 @@ module LinearTraining where
 import Perceptron
 import System.IO.Unsafe
 import System.Random
+import Control.Monad
 
 newtype Line = Line (Int,Int)
 
@@ -21,8 +22,11 @@ isAboveLine [x,y] ln = if y > f ln x
 unsafeGetPoint :: [Float]
 unsafeGetPoint = take 2 (randomRs (1,100) (unsafePerformIO getStdGen))
 
-trainPerceptron :: Perceptron -> Int -> Float -> Perceptron
-trainPerceptron per iters lr = undefined
+trainPerceptron :: Perceptron -> Int -> Line -> Float -> Perceptron
+trainPerceptron per iters ln lr = if iters == 0
+                                  then per
+                                  else let newPer = iteration ln per lr in
+                                  trainPerceptron newPer (iters-1) ln lr
 
 iteration :: Line -> Perceptron -> Float -> Perceptron
 iteration ln per lr = let point = unsafeGetPoint
@@ -31,3 +35,26 @@ iteration ln per lr = let point = unsafeGetPoint
                           delta = expected - actual
                       in
                       adjust per point (fromIntegral delta) lr
+
+verify :: Line -> Perceptron -> IO ()
+verify ln per = do
+  let results = replicate 100 verifyPer
+      correct = length (filter (\r -> r) results)
+  putStrLn $ "The number of correct results is: " ++ (show correct)
+    where verifyPer = let pt = unsafeGetPoint
+                          act = run per pt
+                          expected = isAboveLine pt ln in
+                      act == expected
+  
+
+main :: IO ()
+main = do
+  let per = basicPerceptron 2
+      line = unsafeRandomLine
+  putStrLn $ "The perceptron before training:\n" ++ (show per) 
+  putStrLn "Verifying the perceptron before training"
+  verify line per
+  let trained = trainPerceptron per 1000 line 0.1
+  putStrLn $ "The perceptron after training:\n" ++ (show trained)
+  putStrLn "Verifying the perceptron after training"
+  verify line trained
